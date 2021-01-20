60cc8c67a1ba12bc1a6041353a353b9a34e913da
Nick Qantaria <skott56@outlook.com>
2021-01-20 14:15:56 +0400

    SOLID.InterfaceSegregation

---
+++ SOLID.InterfaceSegregation/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace SOLID.InterfaceSegregation
+{
+    public class Document
+    {
+
+    }
+
+    public interface IPrinter
+    {
+        void Print(Document document);
+    }
+
+    public interface IScanner
+    {
+        void Scan(Document document);
+    }
+
+    public interface IFax
+    {
+        void Fax(Document document);
+    }
+
+    public interface IMultifunctionMachine : IScanner, IPrinter
+    {
+
+    }
+
+    public class MultiFunctionDevice : IMultifunctionMachine
+    {
+        private IPrinter printer;
+        private IScanner scanner;
+
+        public MultiFunctionDevice(IPrinter printer, IScanner scanner)
+        {
+            this.printer = printer;
+            this.scanner = scanner;
+        }
+
+        public void Print(Document document)
+        {
+            printer.Print(document);
+        }
+
+        public void Scan(Document document)
+        {
+            scanner.Scan(document);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
