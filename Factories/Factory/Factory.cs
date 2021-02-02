767403b4f32f20e99f250952418bfdcc36478459
Nick Qantaria <skott56@outlook.com>
2021-02-02 14:06:35 +0400

    Factory, Factory Method

---
+++ Factories/Factory/Factory.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace Factories.Factory
+{
+    //“A factory is an object which is used for creating other objects”. In technical terms, we can say that a factory is a class with a method. That method will create and return different types of objects based on the input parameter, it received.
+
+    public enum CardTypes
+    {
+        TitaniumEdge,
+        MoneyBack,
+        Platinum
+    }
+
+    public class Platinum : CreditCard
+    {
+        public CardTypes CardType()
+        {
+            return CardTypes.Platinum;
+        }
+        public int CreditLimit()
+        {
+            return 35000;
+        }
+        public int AnnualCharge()
+        {
+            return 2000;
+        }
+    }
+
+    public class Titanium : CreditCard
+    {
+        public CardTypes CardType()
+        {
+            return CardTypes.TitaniumEdge;
+        }
+        public int CreditLimit()
+        {
+            return 25000;
+        }
+        public int AnnualCharge()
+        {
+            return 1500;
+        }
+    }
+
+    class MoneyBack : CreditCard
+    {
+        public CardTypes CardType()
+        {
+            return CardTypes.MoneyBack;
+        }
+        public int CreditLimit()
+        {
+            return 15000;
+        }
+        public int AnnualCharge()
+        {
+            return 500;
+        }
+    }
+    public interface CreditCard
+    {
+        CardTypes CardType();
+        int CreditLimit();
+        int AnnualCharge();
+    }
+
+    public class CreditCardFactory
+    {
+        public static CreditCard CreditCard(CardTypes cardType)
+        {
+            CreditCard cardDetails = null;
+
+            if (cardType == CardTypes.MoneyBack)
+                cardDetails = new MoneyBack();
+
+            if (cardType == CardTypes.TitaniumEdge)
+                cardDetails = new Titanium();
+
+            if (cardType == CardTypes.Platinum)
+                cardDetails = new Platinum();
+
+            return cardDetails;
+        }
+    }
+
+    class Factory
+    {
+        static void Main(string[] args)
+        {
+            CreditCard cardDetails = CreditCardFactory.CreditCard(CardTypes.Platinum);
+
+            if (cardDetails != null)
+            {
+                Console.WriteLine("CardType : " + cardDetails.CardType());
+                Console.WriteLine("CreditLimit : " + cardDetails.CreditLimit());
+                Console.WriteLine("AnnualCharge :" + cardDetails.AnnualCharge());
+            }
+
+            else
+                Console.Write("Invalid Card Type");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Factories/FactoryMethod/FactoryMethod.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Factories.FactoryMethod
+{
+    //Define an interface for creating an object, but let the subclasses decide which class to instantiate. The Factory method lets a class defer instantiation it uses to subclasses
+
+    public enum CardTypes
+    {
+        TitaniumEdge,
+        MoneyBack,
+        Platinum
+    }
+
+    public class Platinum : CreditCard
+    {
+        public CardTypes CardType()
+        {
+            return CardTypes.Platinum;
+        }
+        public int CreditLimit()
+        {
+            return 35000;
+        }
+        public int AnnualCharge()
+        {
+            return 2000;
+        }
+    }
+
+    public class Titanium : CreditCard
+    {
+        public CardTypes CardType()
+        {
+            return CardTypes.TitaniumEdge;
+        }
+        public int CreditLimit()
+        {
+            return 25000;
+        }
+        public int AnnualCharge()
+        {
+            return 1500;
+        }
+    }
+
+    class MoneyBack : CreditCard
+    {
+        public CardTypes CardType()
+        {
+            return CardTypes.MoneyBack;
+        }
+        public int CreditLimit()
+        {
+            return 15000;
+        }
+        public int AnnualCharge()
+        {
+            return 500;
+        }
+    }
+    public interface CreditCard
+    {
+        CardTypes CardType();
+        int CreditLimit();
+        int AnnualCharge();
+    }
+
+    public abstract class CreditCardFactory
+    {
+        protected abstract CreditCard MakeProduct();
+        public CreditCard CreateProduct()
+        {
+            return MakeProduct();
+        }
+    }
+
+    public class MoneyBackFactory : CreditCardFactory
+    {
+        protected override CreditCard MakeProduct()
+        {
+            CreditCard product = new MoneyBack();
+            return product;
+        }
+    }
+    public class PlatinumFactory : CreditCardFactory
+    {
+        protected override CreditCard MakeProduct()
+        {
+            CreditCard product = new Platinum();
+            return product;
+        }
+    }
+    public class TitaniumFactory : CreditCardFactory
+    {
+        protected override CreditCard MakeProduct()
+        {
+            CreditCard product = new Titanium();
+            return product;
+        }
+    }
+
+    class FactoryMethod
+    {
+        static void Main(string[] args)
+        {
+            CreditCard creditCard = new PlatinumFactory().CreateProduct();
+
+            if (creditCard != null)
+            {
+                Console.WriteLine("Card Type : " + creditCard.CardType());
+                Console.WriteLine("Credit Limit : " + creditCard.CreditLimit());
+                Console.WriteLine("Annual Charge :" + creditCard.AnnualCharge());
+            }
+
+            else
+                Console.Write("Invalid Card Type");
+
+            Console.WriteLine("--------------");
+
+            creditCard = new MoneyBackFactory().CreateProduct();
+
+            if (creditCard != null)
+            {
+                Console.WriteLine("Card Type : " + creditCard.CardType());
+                Console.WriteLine("Credit Limit : " + creditCard.CreditLimit());
+                Console.WriteLine("Annual Charge :" + creditCard.AnnualCharge());
+            }
+
+            else
+                Console.Write("Invalid Card Type");
+
+            Console.ReadLine();
+        }
+    }
+}
