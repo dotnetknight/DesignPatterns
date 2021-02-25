f8de2b94e5089fd542c57292299e2f1c659e78fd
Nick Qantaria <skott56@outlook.com>
2021-02-25 13:54:53 +0400

    demo 3 singleton

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -139,7 +139,7 @@ namespace Singleton
     }
 }
 
-
+//DEMO2
 
 /*
    public class Foo
@@ -175,6 +175,48 @@ namespace Singleton
                 Console.WriteLine(ReferenceEquals(foo1.Broker, foo2.Broker));
             }
 
+        }
+    }
+ */
+
+
+//DEMO 3
+
+/*
+ public sealed class Singleton
+    {
+        private static int counter = 0;
+        private static Singleton instance = null;
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Singleton();
+                return instance;
+            }
+        }
+        
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter Value " + counter.ToString());
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton fromTeachaer = Singleton.GetInstance;
+            fromTeachaer.PrintDetails("From Teacher");
+            Singleton fromStudent = Singleton.GetInstance;
+            fromStudent.PrintDetails("From Student");
+            Console.ReadLine();
         }
     }
  */
