e560e3468dbb467ad0a873bc11abd8d2f3754d01
Nick Qantaria <skott56@outlook.com>
2021-03-15 11:18:26 +0400

    Adapter pattern

---
+++ Adapter/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Adapter
+{
+    //Adapter is a structural design pattern, which allows incompatible objects to collaborate. The Adapter acts as a wrapper between two objects. It catches calls for one object and transforms them to format and interface recognizable by the second object.
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public decimal Salary { get; set; }
+        public Employee(int id, string name, string designation, decimal salary)
+        {
+            Id = id;
+            Name = name;
+            Designation = designation;
+            Salary = salary;
+        }
+    }
+
+    public class ThirdPartyBillingSystem
+    {
+        //ThirdPartyBillingSystem accepts employees information as a List to process each employee salary
+        public void ProcessSalary(List<Employee> listEmployee)
+        {
+            foreach (Employee employee in listEmployee)
+            {
+                Console.WriteLine(employee.Salary + "$ Salary Credited to " + employee.Name + " Account");
+            }
+        }
+    }
+
+    public interface ITarget
+    {
+        void ProcessCompanySalary(string[,] employeesArray);
+    }
+
+    public class EmployeeAdapter : ITarget
+    {
+        private readonly ThirdPartyBillingSystem thirdPartyBillingSystem = new ThirdPartyBillingSystem();
+
+        public void ProcessCompanySalary(string[,] employeesArray)
+        {
+            string Id = null;
+            string Name = null;
+            string Designation = null;
+            string Salary = null;
+            List<Employee> listEmployee = new List<Employee>();
+
+            for (int i = 0; i < employeesArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < employeesArray.GetLength(1); j++)
+                {
+                    if (j == 0)
+                    {
+                        Id = employeesArray[i, j];
+                    }
+                    else if (j == 1)
+                    {
+                        Name = employeesArray[i, j];
+                    }
+                    else if (j == 1)
+                    {
+                        Designation = employeesArray[i, j];
+                    }
+                    else
+                    {
+                        Salary = employeesArray[i, j];
+                    }
+                }
+
+                listEmployee.Add(new Employee
+                    (Convert.ToInt32(Id),
+                    Name,
+                    Designation,
+                    Convert.ToDecimal(Salary)));
+            }
+
+            Console.WriteLine("Adapter converted Array of Employee to List of Employee");
+            Console.WriteLine("Then delegate to the ThirdPartyBillingSystem for processing the employee salary\n");
+
+            thirdPartyBillingSystem.ProcessSalary(listEmployee);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] employeesArray = new string[5, 4]
+           {
+                {"101","John","SE","10000"},
+                {"102","Smith","SE","20000"},
+                {"103","Dev","SSE","30000"},
+                {"104","Pam","SE","40000"},
+                {"105","Sara","SSE","50000"}
+           };
+
+            ITarget target = new EmployeeAdapter();
+
+            Console.WriteLine("HR system passes employee string array to Adapter\n");
+            target.ProcessCompanySalary(employeesArray);
+
+            Console.Read();
+        }
+    }
+}
