751fc056047dca2467e6429b9d73064870791912
Nick Qantaria <skott56@outlook.com>
2021-06-12 21:49:02 +0400

    Adapter pattern

--- Adapter/Program.cs
+++ Adapter/Program.cs
@@ -23,12 +23,10 @@ namespace Adapter
     public class ThirdPartyBillingSystem
     {
         //ThirdPartyBillingSystem accepts employees information as a List to process each employee salary
-        public void ProcessSalary(List<Employee> listEmployee)
+        public void ProcessSalary(List<Employee> employees)
         {
-            foreach (Employee employee in listEmployee)
-            {
-                Console.WriteLine(employee.Salary + "$ Salary Credited to " + employee.Name + " Account");
-            }
+            employees.ForEach(employee => Console.WriteLine(
+                employee.Salary + "$ Salary Credited to " + employee.Name + " Account"));
         }
     }
 
@@ -82,13 +80,13 @@ namespace Adapter
         static void Main(string[] args)
         {
             string[,] employeesArray = new string[5, 4]
-           {
+            {
                 {"101","John","SE","10000"},
                 {"102","Smith","SE","20000"},
                 {"103","Dev","SSE","30000"},
                 {"104","Pam","SE","40000"},
                 {"105","Sara","SSE","50000"}
-           };
+            };
 
             ITarget target = new EmployeeAdapter();
 
